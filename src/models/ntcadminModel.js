bb8d28c0b070bc272f98fadca91ca55f3347aae1
duvindu0905 <duvindudushan@gmail.com>
2024-12-22 05:24:39 +0530

    remove -d form data set

--- src/models/ntcadminModel.js
+++ src/models/ntcadminModel.js
@@ -1,53 +1,58 @@
+// ntcadminModel.js
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const operatorSchema = new mongoose.Schema({
-    operatorId: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    passWord: {
-        type: String,
-        required: true
-    },
-    fullName: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    nic: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/\S+@\S+\.\S+/, 'is invalid']
-    }
-});
-
-// Automatically remove _id and __v from response
-operatorSchema.set('toJSON', {
-  transform: (doc, ret) => {
-    delete ret._id;  // Remove _id
-    delete ret.__v;  // Remove __v
-    return ret;  // Return the transformed object
+const ntcadminSchema = new Schema({
+  ntcadminId: { 
+    type: Number, 
+    required: true, 
+    unique: true 
   },
+  userName: { 
+    type: String, 
+    required: true, 
+    minlength: 3 
+  },
+  passWord: { 
+    type: String, 
+    required: true, 
+    minlength: 6 
+  },
+  fullName: { 
+    type: String, 
+    required: true 
+  },
+  address: { 
+    type: String, 
+    required: true 
+  },
+  phoneNumber: { 
+    type: Number, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{10}/.test(v);  // Validates a 10-digit phone number
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  nic: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^[0-9]{9}[vV]$/.test(v);  // NIC number validation (9 digits followed by 'v' or 'V')
+      },
+      message: props => `${props.value} is not a valid NIC number!`
+    }
+  },
+  email: { 
+    type: String, 
+    required: true,
+    match: [/\S+@\S+\.\S+/, 'Please use a valid email address.']
+  }
 });
 
-module.exports = mongoose.model('Operator', operatorSchema);
+const Ntcadmin = mongoose.model('Ntcadmin', ntcadminSchema);
+
+module.exports = Ntcadmin;
